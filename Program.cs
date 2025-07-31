dfe35b248e42a702b8b526c59187470da7bf148c
expertpcgroup <99054545+expertpcgroup@users.noreply.github.com>
2025-07-31 18:20:57 -0400

    Update the client access link

--- Program.cs
+++ Program.cs
@@ -19,6 +19,11 @@ builder.Services.AddDbContext<BankingDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(options => options.AddPolicy(name: "NgOrigins", policy =>
+{
+    policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+}));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
